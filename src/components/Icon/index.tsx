40d0b510e32df8b97662c046ad3710b40dea8486
吴梦成 <wumengcheng@wumengchengdeMacBook-Pro.local>
2022-09-05 11:45:28 +0800

    icon

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,7 +1,9 @@
+import type { CSSProperties } from 'vue'
 export interface iconProps {
   className?: string
   type?: 'fixed' | 'copy' | 'close'
   size?: number
+  style?: CSSProperties
 }
 const svgMap = {
   fixed: (
@@ -13,7 +15,6 @@ const svgMap = {
     >
       <path
         d='M334.72 612.16L127.36 404.736l85.12-85.184L390.016 391.04l275.968-214.4-72.32-72.32L678.912 19.2l82.944 82.88 0.192-0.128L928 267.84l-0.192 0.256 82.944 82.944-85.12 85.184-72.32-72.256L638.848 640l71.488 177.408-85.12 85.184-207.488-207.36-255.36 255.296-82.944-82.944 255.36-255.36z'
-        fill='#262626'
         p-id='1441'
       ></path>
     </svg>
@@ -23,7 +24,6 @@ const svgMap = {
       viewBox='64 64 896 896'
       focusable='false'
       data-icon='copy'
-      fill='currentColor'
       aria-hidden='true'
     >
       <path d='M832 64H296c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h496v688c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8V96c0-17.7-14.3-32-32-32zM704 192H192c-17.7 0-32 14.3-32 32v530.7c0 8.5 3.4 16.6 9.4 22.6l173.3 173.3c2.2 2.2 4.7 4 7.4 5.5v1.9h4.2c3.5 1.3 7.2 2 11 2H704c17.7 0 32-14.3 32-32V224c0-17.7-14.3-32-32-32zM350 856.2L263.9 770H350v86.2zM664 888H414V746c0-22.1-17.9-40-40-40H232V264h432v624z'></path>
@@ -44,12 +44,15 @@ const svgMap = {
   ),
 }
 const renderDom = (props: iconProps) => {
-  const { type = 'fixed', size = 12 } = props
+  const { type = 'fixed', size = 12, style = {backgroundColor:"#ddd",fill:"#fff"} } = props
   if (type in svgMap) {
     const svg = svgMap[type]
+    if(size){
+       style.width = size + 'px'
+       style.height = size + 'px'
+    }
     if (svg && svg.props) {
-      svg.props.width = size + 'px'
-      svg.props.height = size + 'px'
+      svg.props.style = style
     }
     return svg
   }
