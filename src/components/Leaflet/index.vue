91e915e0c099a136d8e7852f500e0adca7f54a35
吴梦成 <wumengcheng@192.168.10.2>
2024-11-11 23:58:07 +0800

    add image

--- src/components/Leaflet/index.vue
+++ src/components/Leaflet/index.vue
@@ -1,29 +1,53 @@
 <template>
-    <div id="map" style="height: 400px; width: 100%;"></div>
+    <div class="home-view">
+        <div class="map-layer" id="map"></div>
+    </div>
+
 </template>
 
 <script lang="ts" setup>
 import { onMounted, ref } from 'vue';
-import L, { Map } from 'leaflet';
-
-const map = ref<Map | null>(null); // 用 Map 类型定义 map
+import L from 'leaflet';
 
 onMounted(() => {
+    const bounds = L.latLngBounds(L.latLng(0, 0), L.latLng(-256, 256))
     // 初始化地图
-    map.value = L.map('map').setView([51.505, -0.09], 13);
+    let map = L.map('map', {
+        center: [-102, 148],
+        maxBounds: bounds,
+        crs: L.CRS.Simple,
+        attributionControl: false,
+        zoomControl: false,
+        maxZoom: 7,
+        minZoom: 4,
+        zoom: 5
 
-    // 加载底图
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map.value);
+    })
+
+    L.tileLayer('images/map/{z}/{x}/{y}.png', {
+        minZoom: 4,
+        maxZoom: 7,
+        noWrap: true
+    }).addTo(map)
+
+
 
 
 });
 </script>
 
 <style>
-#map {
-    height: 100%;
+.home-view {
+    position: relative;
+    height: 100vh;
+    width: 100vw;
+}
+
+.map-layer {
+    position: absolute;
+    left: 0;
+    top: 0;
     width: 100%;
+    height: 100%;
 }
 </style>
